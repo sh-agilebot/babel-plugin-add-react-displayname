4fe781844baa255cb48dedfec08e60bdd6a9abdc
kokororin <i@kotori.love>
2023-12-19 14:02:36 +0800

    fix: display name

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function transform (babel) {
           setDisplayNameAfter(path, path.node.id, babel.types)
         }
       },
-      FunctionDeclaration: function (path, state) {        
+      FunctionDeclaration: function (path, state) {
         if (doesReturnJSX(path.node.body) || (path.node.id && path.node.id.name &&
                                               isKnownComponent(path.node.id.name, state.opts.knownComponents))) {
           var displayName
@@ -18,10 +18,10 @@ function transform (babel) {
               // An anonymous function declaration in export default declaration.
               // Transform `export default function () { ... }`
               // to `var _uid1 = function () { .. }; export default __uid;`
-              // then add displayName to _uid1  
-              var extension = pathMod.extname(state.file.opts.filename) 
+              // then add displayName to _uid1
+              var extension = pathMod.extname(state.file.opts.filename)
               var name = pathMod.basename(state.file.opts.filename, extension)
-              
+
               var id = path.scope.generateUidIdentifier("uid");
               path.node.id = id
               displayName = name
@@ -66,7 +66,7 @@ function shouldSetDisplayNameForFuncExpr(path, knownComponents) {
   // Parent must be either 'AssignmentExpression' or 'VariableDeclarator' or 'CallExpression' with a parent of 'VariableDeclarator'
   var id
   if (path.parentPath.node.type === 'AssignmentExpression' &&
-      path.parentPath.node.left.type !== 'MemberExpression' && // skip static members 
+      path.parentPath.node.left.type !== 'MemberExpression' && // skip static members
       path.parentPath.parentPath.node.type == 'ExpressionStatement' &&
       path.parentPath.parentPath.parentPath.node.type == 'Program') {
     id = path.parentPath.node.left
@@ -82,7 +82,7 @@ function shouldSetDisplayNameForFuncExpr(path, knownComponents) {
           path.parentPath.parentPath.parentPath.node.type === 'Program') {
         id = path.parentPath.node.id
       }
-    } 
+    }
   }
 
   if (id) {
@@ -142,7 +142,9 @@ function doesReturnJSX (body) {
     var lastBlock = block.slice(0).pop()
 
     if (lastBlock.type === 'ReturnStatement') {
-      return lastBlock.argument !== null && lastBlock.argument.type === 'JSXElement'
+      return lastBlock.argument !== null && (lastBlock.argument.type === 'JSXElement' ||
+        (lastBlock.argument.type === 'ConditionalExpression' &&
+          (lastBlock.argument.consequent.type === 'JSXElement' || lastBlock.argument.alternate.type === 'JSXElement')))
     }
   }
 
@@ -172,6 +174,6 @@ function setDisplayNameAfter(path, nameNodeId, t, displayName) {
       t.stringLiteral(displayName)
     ))
 
-    blockLevelStmnt.insertAfter(setDisplayNameStmn)    
+    blockLevelStmnt.insertAfter(setDisplayNameStmn)
   }
 }
